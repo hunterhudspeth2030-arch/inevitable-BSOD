00bc5259df4658f7e3789ba8a28cc4bd9c0c0b04
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-05 15:23:15 -0800

    Update Invoke-BSOD.bat

--- Invoke-BSOD.bat
+++ Invoke-BSOD.bat
@@ -1,10 +1,12 @@
 @echo off
 rem This line allows the crash to occur without a prompt
 reg add HKLM
-reg add HKLM\bsod\ControlSet001\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 1 /f
-reg add HKLM\bsod\ControlSet001\Control\CrashControl /v ForceCrash /t REG_DWORD /d 1 /f
+@echo off
+REM Enable Crash Dumps
+reg add "HKLM\BSOD\ControlSet001\Control\CrashControl" /v CrashDumpEnabled /t REG_DWORD /d 1 /f
+REM Enable Forced Crash (Blue Screen)
+reg add "HKLM\BSOD\ControlSet001\Control\CrashControl" /v ForceCrash /t REG_DWORD /d 1 /f
+echo Registry keys updated successfully.
 echo "have fun with the crash :)"
 start /wait notepad.exe
-
-
 
