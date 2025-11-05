4a0d628f1ca39260fe021da2537392be01e8d044
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-05 15:36:14 -0800

    Update Invoke-BSOD.bat

--- Invoke-BSOD.bat
+++ Invoke-BSOD.bat
@@ -2,7 +2,9 @@
 rem This line allows the crash to occur without a prompt
 reg add HKLM
 @echo off
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\CrashControl" /v CrashDumpEnabled /t REG_DWORD /d 1 /f && reg add "HKLM\SYSTEM\CurrentControlSet\Control\CrashControl" /v ForceCrash /t REG_DWORD /d 1 /f
+ reg add "HKLM\SYSTEM\CurrentControlSet\Control\CrashControl" /v ForceCrash /t REG_DWORD /d 1 /f
+ 
+ reg add "HKLM\SYSTEM\CurrentControlSet\Control\CrashControl" /v CrashDumpEnabled /t REG_DWORD /d 1 /f
 echo "have fun with the crash :)"
 start /wait notepad.exe
 
