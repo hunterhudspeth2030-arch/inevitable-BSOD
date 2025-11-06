e7835649bfacb9f8b0321a822413fc80054ba08e
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-05 17:06:06 -0800

    Delete Invoke-BSOD.bat

--- Invoke-BSOD.bat
+++
@@ -1,8 +0,0 @@
-
-
-
-@echo off
-:: Enable BSOD via Ctrl+Scroll (USB)
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdhid\Parameters" /v CrashOnCtrlScroll /t REG_DWORD /d 1 /f
-echo Registry key added. Press Ctrl+Scroll Lock twice to trigger BSOD (USB keyboard).
-pause
